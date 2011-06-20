8207e185f0fc05738a094f52e6b98aaf66d4660b
Zachary Dremann <dremann@gmail.com>
2011-06-20 18:34:09 +0000

    First Move to SVN

---
+++ ChangableConstants.h
@@ -0,0 +1,22 @@
+#ifndef CHANGEABLE_CONSTANTS_H
+#define CHANGEABLE_CONSTANTS_H
+
+static bool const runAutonMode = false;	// If you want to disable autonomous mode, set this to false
+
+static bool const liftStartsAtTop = false;// If the lift starts at the top, set to true, if lift starts at bottom, set to false
+
+static int const whiteLevel = 500;			// level where line is recognized as white.
+
+static int const maxLiftClicks = 750;	// clicks from bottom to top of lift
+
+static int const partwayLiftClicks = 250; // clicks from bottom to mid-level (used for lowering rings onto posts)
+
+static int const turnAmount = 1200;			// determines how far robot will turn in autonomous mode after finding the line.
+																				//		try to get the robot to turn about 90 degrees
+
+static int const shakeTime = 7000;			// determines the number of milliseconds the robot will shake after turning, trying to find the line again. (1000 ms = 1 sec)
+
+static int const clawClosedPos = 55;
+
+static int const clawOpenPos = -10;
+#endif
---
+++ ZHeader.h
@@ -0,0 +1,41 @@
+#ifndef Z_HEADER_H
+#define Z_HEADER_H
+
+#define leftMotor motor[motor_L]
+#define rightMotor motor[motor_R]
+#define liftMotor1 motor[motor_lift_L]
+#define liftMotor2 motor[motor_lift_R]
+#define liftMotors(x) motor[motor_lift_L]=motor[motor_lift_R]=(x)
+#define liftValue motor[motor_lift_L]
+#define clawL motor[servo_claw_L]
+#define clawR motor[servo_claw_R]
+
+#define cont1 vexRT[Ch1]
+#define cont2 vexRT[Ch2]
+#define cont3 vexRT[Ch3]
+#define cont4 vexRT[Ch4]
+
+#define cont5U vexRT[Btn5U]
+#define cont5D vexRT[Btn5D]
+#define cont6U vexRT[Btn6U]
+#define cont6D vexRT[Btn6D]
+#define cont7L vexRT[Btn7L]
+#define cont7R vexRT[Btn7R]
+
+/*
+ * uncomment for old controler
+#define cont5U ((vexRT[Ch5]>0)?127:0)
+#define cont5D ((vexRT[Ch5]<0)?127:0)
+#define cont6U ((vexRT[Ch6]>0)?127:0)
+#define cont6D ((vexRT[Ch6]<0)?127:0)
+#define cont7L ((vexRT[Ch4]>0)?127:0)
+#define cont7R ((vexRT[Ch4]<0)?127:0)
+*/
+
+#define liftENC SensorValue[enc_lift]
+#define clawBump SensorBoolean[bump_claw]
+#define lineL SensorValue(lineFollower_L)
+#define lineR SensorValue(lineFollower_R)
+
+const static int maxMotor = 127;
+#endif

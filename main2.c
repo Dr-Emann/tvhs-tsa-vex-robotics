4a030c08a976f6b9816c4e059bbde146c3110f8b
Zachary Dremann <dremann@gmail.com>
2011-06-20 20:00:53 +0000

    IMPORTANT: Changed lift to use controller buttons 8U, 8R, and 8D, to fully lift, partway drop, and fully drop, respectively
    Updated some comments

--- main2.c
+++ main2.c
@@ -37,21 +37,24 @@ task main()
   int joy_y;            // will hold the Y value of the analog stick (choices below)
   while(true)
   {
-    joy_x = cont1;   // This is the RIGHT analog stick.  For LEFT, change 'Ch1' to 'Ch4'.
-    joy_y = cont2;   // This is the RIGHT analog stick.  For LEFT, change 'Ch2' to 'Ch3'.
-
+    joy_x = cont1;   // This is the RIGHT analog stick.  For LEFT, change 'cont1' to 'cont4'.
+    joy_y = cont2;   // This is the RIGHT analog stick.  For LEFT, change 'cont2' to 'cont3'.
 
     setMovement(joy_x,joy_y);
 
-    if(cont5U>0)
+    // lift control
+    if(cont8U>0)
       StartTask(lift);
-    else if(cont5D>0)
+    else if(cont8D>0)
     	StartTask(lower);
-    //kill switch
+    else if(cont8R>0)
+    	StartTask(lowerPartway);
+    // claw control
     if(cont7L>0)
     	openClaw();
     else if(cont7R>0)
     	closeClaw();
+    // manual override for lift
     if(cont3>120)
     {
     	liftMotors(50);
@@ -60,6 +63,7 @@ task main()
     {
     	liftMotors(-50);
     }
+    //kill switch
     if(cont6D>0)
     {
       StopTask(lift);

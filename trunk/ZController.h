38edeba9d08a70287e79adb957a35a98ed0faef4
dremann@gmail.com <dremann@gmail.com@dad576cb-41da-8d2f-7aee-3a2ef2e68bd9>
2011-06-20 21:43:58 +0000

    Split file, to allow easy access to all functions requiring access to the VexRT controller. All future functions requiring controller access will be placed in ZController.h

---
+++ trunk/ZController.h
@@ -0,0 +1,39 @@
+#ifndef Z_CONTROLLER_H
+#define Z_CONTROLLER_H 1
+
+void mainLoop()
+{
+	setMovement(cont1,cont2);
+
+	// lift control
+	if(cont8U>0)
+		StartTask(lift);
+	else if(cont8D>0)
+		StartTask(lower);
+	else if(cont8R>0)
+		StartTask(lowerPartway);
+	// claw control
+	if(cont7L>0)
+		openClaw();
+	else if(cont7R>0)
+		closeClaw();
+	// manual override for lift
+	if(cont3>120)
+	{
+		liftMotors(50);
+	}
+	else if(cont3<-120)
+	{
+		liftMotors(-50);
+	}
+	//kill switch
+	if(cont6D>0)
+	{
+		StopTask(lift);
+		StopTask(lower);
+		StopTask(openClaw);
+		StopTask(closeClaw);
+		stopAllMotors();
+	}
+}
+#endif
